9f768d2dbf472f6031079b9c061d4be11a96dfc1
Harsha-C2508 <harshac2508@gmail.com>
2022-07-17 00:39:37 +0530

    new branch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Weass from './component/Weass';
+
+function App() {
+  return (
+    <div className="App">
+        <Weass/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Weass.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Heading, Stack, Text } from '@chakra-ui/react'
+const Weass = () => {
+  return (
+    <>
+    <Heading>Text Theming</Heading>
+    <Stack>
+    <Text fontSize='xs'> In love with React & Next</Text>
+    <Text fontSize='sm'> In love with React & Next</Text>
+    <Text fontSize='md'> In love with React & Next</Text>
+    <Text fontSize='lg'> In love with React & Next</Text>
+    <Text fontSize='xl'> In love with React & Next</Text>
+    </Stack>
+    </>
+  )
+}
+
+export default Weass;
